e5398eb99429c2400c0e3bc66104320b00b25845
Emir Ali <>
2024-02-14 15:03:28 -0300

    fix: arreglo error se searchparams

--- src/app/(index)/layout.tsx
+++ src/app/(index)/layout.tsx
@@ -1,11 +1,12 @@
 import { Footer } from '@/components/footer';
 import { Header } from '@/components/header';
+import { Suspense } from 'react';
 
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      {children}
+      <Suspense>{children}</Suspense>
       <Footer />
     </>
   );
