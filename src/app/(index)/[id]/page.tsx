e77cf3d274c284af4f4383cd171e722a296ffbe2
Emir Ali <>
2024-02-14 19:34:56 -0300

    añado product detail

--- src/app/(index)/[id]/page.tsx
+++ src/app/(index)/[id]/page.tsx
@@ -1,5 +1,9 @@
 import { api } from '@/api';
+import { Breadcum } from '@/components/breadcum';
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
+import { BASE_URL } from '@/lib';
 import { Metadata } from 'next';
+import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
 interface Props {
@@ -14,7 +18,23 @@ export async function generateMetadata({ params: { id } }: Props): Promise<Metad
 
   return {
     title: product.title,
-    description: product.description
+    description: product.description,
+    openGraph: {
+      type: 'article',
+      description: product.description,
+      title: product.title,
+      tags: product.category.name,
+      url: `${BASE_URL}/${product.id}`,
+      images: [
+        {
+          url: product.images[0],
+          alt: product.title,
+          type: 'image/jpeg',
+          width: 800,
+          height: 600
+        }
+      ]
+    }
   };
 }
 
@@ -24,8 +44,44 @@ export default async function ProductPage({ params: { id } }: Props) {
   if (!product) notFound();
 
   return (
-    <div>
-      <h1>Hello Page {id}</h1>
+    <div className='mx-auto flex w-2/3 flex-col p-5'>
+      <Breadcum
+        paths={[
+          {
+            href: '/',
+            name: 'Home'
+          },
+          {
+            href: `/search?category=${product.category.id}`,
+            name: product.category.name
+          },
+          {
+            href: `/${product.id}`,
+            name: product.title
+          }
+        ]}
+      />
+      <div className='grod-cols-1 grid w-full md:grid-cols-2 gap-10 mt-8'>
+        <div className='flex flex-col'>
+          <Carousel>
+            <CarouselContent>
+              {product.images.map((image, index) => {
+                return (
+                  <CarouselItem key={index}>
+                    <Image
+                      src={image}
+                      alt={product.title}
+                      width={800}
+                      height={600}
+                      className='h-full w-full rounded-xl'
+                    />
+                  </CarouselItem>
+                );
+              })}
+            </CarouselContent>
+          </Carousel>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/breadcum/breadcum.tsx
@@ -0,0 +1,34 @@
+import { cn } from '@/lib';
+import Link from 'next/link';
+import React from 'react';
+
+interface Props {
+  paths: {
+    name: string;
+    href: string;
+  }[];
+}
+
+export const Breadcum = ({ paths }: Props) => {
+  return (
+    <div className='flex items-center space-x-2 text-sm text-gray-500'>
+      {paths.map((path, index) => (
+        <div key={path.href} className='flex flex-row items-center justify-start'>
+          <Link
+            className={cn('hover:underline', {
+              'text-accent-foreground': index === paths.length - 1
+            })}
+            href={path.href}
+            passHref={true}
+            prefetch={false}
+            shallow={true}
+            aria-label='breadcrumb'
+          >
+            {path.name}
+          </Link>
+          {index !== paths.length - 1 && <span className='ml-2 text-xs'>{'>'}</span>}
+        </div>
+      ))}
+    </div>
+  );
+};
