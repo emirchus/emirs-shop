c4bcb91fccafdf5f0f7fc60d4ab6789643c34844
Emir Ali <>
2024-02-15 20:35:35 -0300

    feat: added dashboard

--- src/lib/subdomain.ts
+++ src/lib/subdomain.ts
@@ -1,12 +1,27 @@
-export type Subdomain = 'www' | 'admin';
-
-export const APPS: Record<Subdomain, string> = {
-  www: '/',
-  admin: '/admin'
-};
+export const APPS : {
+  subdomain: string;
+  path: string;
+}[]
+ = [
+  {
+    subdomain: 'www',
+    path: '/store/'
+  },
+  {
+    subdomain: 'admin',
+    path: '/admin/'
+  }
+];
 
 export function getSubdomain(location: string) {
   const locationSegments = location.split('.');
 
-  return locationSegments.slice(0, -1).join('');
+  let sliceTill = -2;
+
+  // Path localhost
+  const isLocalhost = locationSegments.slice(-1)[0] === 'localhost';
+
+  if (isLocalhost) sliceTill = -1;
+
+  return locationSegments.slice(0, sliceTill).join('');
 }
