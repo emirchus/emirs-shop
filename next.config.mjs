d2c09ce8073a8561ae1e37abb0dd26025c913bbf
Emir Ali <>
2024-02-15 19:07:43 -0300

    added my own site to showcase

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
         has: [
             {
                 type: 'host',
-                value: 'admin.emirs-shop.vercel.app',
+                value: 'admin',
             },
         ],
         destination: '/admin/:path*',
